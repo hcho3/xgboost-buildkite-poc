678bc0e8cc1683f4640b430b5b1a358081d56640
Hyunsu Cho <chohyu01@cs.washington.edu>
2022-07-26 01:04:23 -0700

    Fix permission of CLI executable

--- tests/buildkite/test-python-cpu.sh
+++ tests/buildkite/test-python-cpu.sh
@@ -9,5 +9,6 @@ source tests/buildkite/conftest.sh
 mkdir -pv python-package/dist
 buildkite-agent artifact download "python-package/dist/*.whl" . --step build-cuda
 buildkite-agent artifact download "xgboost" . --step build-cpu
+chmod +x ./xgboost
 
 tests/ci_build/ci_build.sh cpu docker tests/ci_build/test_python.sh cpu
